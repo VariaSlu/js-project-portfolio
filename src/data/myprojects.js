667bc1d400de85f8013f4d7a36e5d45308b577b2
VariaSlu <varvaraslugina@gmail.com>
2025-05-02 14:35:19 +0200

    image links

--- src/data/myprojects.js
+++ src/data/myprojects.js
@@ -1,8 +1,7 @@
-// myprojects.js
 export const myprojects = [
   {
     name: "Personal Portfolio",
-    image: "https://images.unsplash.com/photo-1520792532857-293bd046307a?auto=format&fit=crop&w=600&q=80",
+    image: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     goal: "Build my very own portfolio! The foundation of the project built over two weeks as I start to learn about two different CSS libraries: Tailwind and Styled Components.",
     role: "Build and style my digital home, animation on scroll and other features",
     tags: ["HTML5", "CSS3", "React", "Personal project", "CSS libraries", "Will power"],
@@ -14,7 +13,7 @@ export const myprojects = [
   },
   {
     name: "Multiple-choice quiz",
-    image: "https://images.unsplash.com/photo-1520792532857-293bd046307a?auto=format&fit=crop&w=600&q=80",
+    image: "https://images.unsplash.com/photo-1618472609777-b038f1f04b8d?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     goal: "Create a user-friendly experience that works for everyone, including those using screen readers, keyboards, and other assistive technologies.",
     role: "Foundational HTML structure, tab-focus and arrow-key controls for a fully accessible user experience.",
     tags: ["HTML5", "CSS3", "React", "WAI-ARIA", "Lighthouse", "WCAG"],
@@ -26,38 +25,38 @@ export const myprojects = [
   },
   {
     name: "Weather app",
-    image: "https://images.unsplash.com/photo-1520792532857-293bd046307a?auto=format&fit=crop&w=600&q=80",
+    image: "https://images.unsplash.com/photo-1620207418302-439b387441b0?q=80&w=3067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     goal: "Group project using TypeScript resulted in a working weather app that uses a weather API to tell today's weather and temperature.",
     role: "I worked with the OpenWeatherMap API to craft a clear and meaningful 4-day forecast experience for users.",
     tags: ["HTML5", "CSS3", "TypeScript", "APIs"],
     links: {
       process: "#process-link",
       code: "https://github.com/VariaSlu",
-      demo: "https://example.netlify.app"
+      demo: "https://heatherweather.netlify.app"
     }
   },
   {
     name: "Recipe app",
-    image: "https://images.unsplash.com/photo-1520792532857-293bd046307a?auto=format&fit=crop&w=600&q=80",
+    image: "https://images.unsplash.com/photo-1641160923894-b1a80920187d?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     goal: "working with APIs to fetch external data to our App.",
     role: "fetched real recipes from API, dynamically updated the UI, handled API limits. Adjust filtering and sorting logic to match the new API response format. Deal with missing or inconsistent data.",
     tags: ["HTML5", "CSS3", "JavaScript", "APIs", "Personal project"],
     links: {
       process: "#process-link",
       code: "https://github.com/VariaSlu",
-      demo: "https://example.netlify.app"
+      demo: "https://recipelibrarybyvaria.netlify.app"
     }
   },
   {
     name: "Tarot for developers",
-    image: "https://images.unsplash.com/photo-1520792532857-293bd046307a?auto=format&fit=crop&w=600&q=80",
+    image: "https://images.unsplash.com/photo-1657458891294-7380eb30d0a1?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     goal: "project helpd me get a little bit of context on how developers think while learning new coding skills and building my first functional web experience.",
     role: "learn and use VS code, GitHub and other tools first time.",
     tags: ["HTML5", "CSS3", "Personal project"],
     links: {
       process: "#process-link",
       code: "https://github.com/VariaSlu",
-      demo: "https://example.netlify.app"
+      demo: "https://tarotfordevelopers.netlify.app"
     }
   }
 ];
